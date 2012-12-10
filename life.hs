973942558b78362491a9ec5b512ccf4ac5b25d5a
inaimathi <inaimathi@lambda>
2012-12-10 11:27:11 -0500

    -Haskell solution now uses Data.Set (in theory, Data.Set.member should be much faster than Data.List.elem, so this should accelerate both printWorld and lifeStep)

--- life.hs
+++ life.hs
@@ -1,36 +1,38 @@
 module Main where
-import Data.List
+import Data.List (group, sort, concatMap)
+import Data.Set
 
-lifeStep :: [(Int, Int)] -> [(Int, Int)]
-lifeStep cells = [head g | g <- grouped cells, viable g]
-  where grouped = group . sort . concat . map neighbors
+lifeStep :: Set (Int, Int) -> Set (Int, Int)
+lifeStep cells = fromList [head g | g <- grouped cells, viable g]
+  where grouped = group . sort . concatMap neighbors . toList
         neighbors (x, y) = [(x+dx, y+dy) | dx <- [-1..1], dy <- [-1..1], (dx,dy) /= (0,0)]
         viable [_,_,_] = True
-        viable [c,_] = c `elem` cells
+        viable [c,_] = c `member` cells
         viable _ = False
 
-
-showWorld :: [(Int, Int)] -> IO ()
+showWorld :: Set (Int, Int) -> IO ()
 showWorld cells = mapM_ putStrLn $ worldToGrid cells
   where worldToGrid cells = [[cellChar (x, y) | x <- [least..greatest]] | y <- [least..greatest]]
-        cellChar cell = if cell `elem` cells then '#' else '.'
+        cellChar cell = if cell `member` cells then '#' else '.'
         (least, greatest) = worldBounds cells
 
+worldBounds :: Set (Int, Int) -> (Int, Int)
 worldBounds cells = (least, greatest)
-  where least = min x y
+  where lst = toList cells
+        least = min x y
         greatest = max x' y'
-        (x, y) = head cells
-        (x', y') = last cells
+        (x, y) = head lst
+        (x', y') = last lst
 
-runLife :: Int -> [(Int, Int)] -> IO ()
+runLife :: Int -> Set (Int, Int) -> IO ()
 runLife steps cells = rec (steps - 1) cells
   where rec 0 cells = showWorld cells
         rec s cells = do showWorld cells
                          putStrLn ""
                          rec (s - 1) $ lifeStep cells
 
-glider = [(1, 0), (2, 1), (0, 2), (1, 2), (2, 2)]
-blinker = [(1, 0), (1, 1), (1, 2)]
+glider = fromList [(1, 0), (2, 1), (0, 2), (1, 2), (2, 2)]
+blinker = fromList [(1, 0), (1, 1), (1, 2)]
 
 main :: IO ()
 main = do
